d59670ab53a7cd2fd5795e33290598ad400c75af
Mike Stromme <mstromme@suburbanenterprises.com>
2023-07-28 11:49:11 -0500

    x

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from gtts import gTTS
 from feedgen.feed import FeedGenerator
 import os
 import datetime
+import pytz
 
 
 
@@ -22,7 +23,6 @@ def text_to_audio(text, output_file):
 
 
 
-
 def create_rss_feed(title, link, description, audio_url, pub_date):
     fg = FeedGenerator()
     fg.title(title)
@@ -32,12 +32,18 @@ def create_rss_feed(title, link, description, audio_url, pub_date):
     fe = fg.add_entry()
     fe.title(title)
     fe.link(href=audio_url)
-    fe.pubdate(pub_date)
+
+    # Convert pub_date to a timezone-aware datetime object (using UTC timezone in this example)
+    utc_timezone = pytz.utc
+    pub_date_with_tz = pub_date.astimezone(utc_timezone)
+
+    fe.pubdate(pub_date_with_tz)
 
     return fg.rss_str(pretty=True)
 
+
 def main():
-    article_url = "https://example.com/article"
+    article_url = "https://news.crunchbase.com/web3/crypto-blockchain-startup-vc-funding-falling-data/?utm_source=cb_weekend&utm_medium=email&utm_campaign=20230722"
     audio_file = "output.mp3"
     podcast_title = "My Podcast Title"
     podcast_description = "Description of my podcast."
@@ -50,14 +56,17 @@ def main():
     text_to_audio(article_content, audio_file)
 
     # Step 3: Generate RSS feed
-    current_datetime = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")
+    utc_timezone = pytz.utc
+    current_datetime = datetime.datetime.now(tz=utc_timezone)
+
     rss_content = create_rss_feed(podcast_title, article_url, podcast_description, audio_file, current_datetime)
 
     # Step 4: Save the RSS feed to a file
-    with open(rss_file, "w", encoding="utf-8") as f:
-        f.write(rss_content)
+    with open(rss_file, "wb") as f:  # Use "wb" for writing bytes
+        f.write(rss_content)  # Write the bytes object directly
 
     print("Podcast generated successfully!")
+
 
 if __name__ == "__main__":
     main()
